db80463c2a1007d616e9d6150b04294e2ee74618
nothingnew <tfiolka91@o2.pl>
2014-08-20 13:30:03 +0200

    nowe menu - mozg rozjebany

--- com/example/menu/MainActivity.java
+++ com/example/menu/MainActivity.java
@@ -10,6 +10,7 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.WakeLockOptions;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.modifier.MoveByModifier;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
@@ -44,7 +45,9 @@ public class MainActivity extends BaseGameActivity {
 	//private Sound mSound;
 	private Font mFont;
 	private ITextureRegion mTextureRegionBackground;
+	private ITextureRegion mTextureRegionBrain[];
 	private String mMenuString;
+	private Sprite brainSprite[];
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -71,10 +74,19 @@ public class MainActivity extends BaseGameActivity {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/menu/");
 
 		BuildableBitmapTextureAtlas mBitmapTextureAtlas1 = new BuildableBitmapTextureAtlas(
-				mEngine.getTextureManager(), 800, 300);
+				mEngine.getTextureManager(), 1024, 768);
 
 		mTextureRegionBackground = BitmapTextureAtlasTextureRegionFactory
 				.createFromAsset(mBitmapTextureAtlas1, this, "background.png");
+		mTextureRegionBrain = new ITextureRegion[4];
+		mTextureRegionBrain[0] = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(mBitmapTextureAtlas1, this, "left.png");
+		mTextureRegionBrain[1] = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(mBitmapTextureAtlas1, this, "top.png");
+		mTextureRegionBrain[2] = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(mBitmapTextureAtlas1, this, "right.png");
+		mTextureRegionBrain[3] = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(mBitmapTextureAtlas1, this, "bottom.png");
 
 		try {
 			mBitmapTextureAtlas1
@@ -128,9 +140,10 @@ public class MainActivity extends BaseGameActivity {
 
 		// nie dziala, leci error
 		// mSound.play();
-
-		Sprite menuSprite = new Sprite(WIDTH / 2, HEIGHT / 2,
-				mTextureRegionBackground, getVertexBufferObjectManager()) {
+		
+		brainSprite = new Sprite[4];
+		brainSprite[0] = new Sprite(WIDTH / 2 - 250, HEIGHT / 2,
+				mTextureRegionBrain[0], getVertexBufferObjectManager()) {
 
 			@Override
 			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
@@ -139,14 +152,11 @@ public class MainActivity extends BaseGameActivity {
 				int eventAction = pSceneTouchEvent.getAction();
 
 				switch (eventAction) {
-				case TouchEvent.ACTION_UP: {
-					// mSound.stop();
-					if (pSceneTouchEvent.getX() > WIDTH / 2) // right
-						startActivity(new Intent(pContext,
-								com.example.imagequiz.MainActivity.class));
-					else // left
-						startActivity(new Intent(pContext,
-								com.example.guesswhat.MainActivity.class));
+				case TouchEvent.ACTION_DOWN: {
+					System.out.println("left brain clicked");
+					startActivity(new Intent(pContext,
+							com.example.imagequiz.MainActivity.class));
+					animateBrain();
 					break;
 				}
 				default:
@@ -156,21 +166,138 @@ public class MainActivity extends BaseGameActivity {
 				return true;
 			}
 		};
+		brainSprite[1] = new Sprite(WIDTH / 2, HEIGHT / 2 + 150,
+				mTextureRegionBrain[1], getVertexBufferObjectManager()) {
 
-		Text menuText = new Text(menuSprite.getWidth() / 2,
-				menuSprite.getHeight() / 2, mFont, mMenuString, 100,
-				getVertexBufferObjectManager());
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
 
-		menuText.setHorizontalAlign(HorizontalAlign.CENTER);
+				int eventAction = pSceneTouchEvent.getAction();
 
-		menuSprite.attachChild(menuText);
+				switch (eventAction) {
+				case TouchEvent.ACTION_DOWN: {
+					System.out.println("top brain clicked");
+					animateBrain();
+					break;
+				}
+				default:
+					break;
+				}
 
-		pScene.attachChild(menuSprite);
+				return true;
+			}
+		};
+		brainSprite[2] = new Sprite(WIDTH / 2 + 250, HEIGHT / 2,
+				mTextureRegionBrain[2], getVertexBufferObjectManager()) {
 
-		pScene.registerTouchArea(menuSprite);
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+
+				int eventAction = pSceneTouchEvent.getAction();
+
+				switch (eventAction) {
+				case TouchEvent.ACTION_DOWN: {
+					System.out.println("right brain clicked");
+					startActivity(new Intent(pContext,
+							com.example.guesswhat.MainActivity.class));
+					animateBrain();
+					break;
+				}
+				default:
+					break;
+				}
+
+				return true;
+			}
+		};
+		brainSprite[3] = new Sprite(WIDTH / 2, HEIGHT / 2 - 150,
+				mTextureRegionBrain[3], getVertexBufferObjectManager()) {
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+
+				int eventAction = pSceneTouchEvent.getAction();
+
+				switch (eventAction) {
+				case TouchEvent.ACTION_DOWN: {
+					System.out.println("bottom brain clicked");
+					startActivity(new Intent(pContext,
+							com.example.findwords.MainActivity.class));
+					animateBrain();
+					break;
+				}
+				default:
+					break;
+				}
+
+				return true;
+			}
+		};
+		
+
+//		Sprite menuSprite = new Sprite(WIDTH / 2, HEIGHT - 150,
+//				mTextureRegionBackground, getVertexBufferObjectManager()) {
+//
+//			@Override
+//			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+//					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+//
+//				int eventAction = pSceneTouchEvent.getAction();
+//
+//				switch (eventAction) {
+//				case TouchEvent.ACTION_UP: {
+//					// mSound.stop();
+//					if (pSceneTouchEvent.getX() > WIDTH / 2) // right
+//						startActivity(new Intent(pContext,
+//								com.example.imagequiz.MainActivity.class));
+//					else // left
+//						startActivity(new Intent(pContext,
+//								com.example.guesswhat.MainActivity.class));
+//					break;
+//				}
+//				default:
+//					break;
+//				}
+//
+//				return true;
+//			}
+//		};
+//
+//		Text menuText = new Text(menuSprite.getWidth() / 2,
+//				menuSprite.getHeight() / 2, mFont, mMenuString, 100,
+//				getVertexBufferObjectManager());
+//
+//		menuText.setHorizontalAlign(HorizontalAlign.CENTER);
+//
+//		menuSprite.attachChild(menuText);
+//
+//		pScene.attachChild(menuSprite);
+//		pScene.registerTouchArea(menuSprite);
+		
+		for (int i = 0; i < 4; ++i)
+		{
+			pScene.attachChild(brainSprite[i]);
+			pScene.registerTouchArea(brainSprite[i]);
+		}
 
 		pScene.setTouchAreaBindingOnActionDownEnabled(true);
 
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
+	}
+	
+	public void animateBrain() {
+		
+		MoveByModifier moveModifier[] = new MoveByModifier[4];
+		
+		moveModifier[0] = new MoveByModifier(1.5f, 90f, -35f);
+		moveModifier[1] = new MoveByModifier(1.5f, -5f, -100f);
+		moveModifier[2] = new MoveByModifier(1.5f, -90f, 0f);
+		moveModifier[3] = new MoveByModifier(1.5f, 30f, 50f);
+		
+		for (int i = 0; i < 4; ++i)
+			brainSprite[i].registerEntityModifier(moveModifier[i]);
 	}
 }
